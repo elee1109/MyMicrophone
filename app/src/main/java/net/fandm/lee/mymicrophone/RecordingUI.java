727263aa388ff29f8e41ffee65712abb168ae5ab
elee1109 <elee2@fandm.edu>
2023-02-23 13:04:43 -0500

    icon added -- make custom layout to-do

--- app/src/main/java/net/fandm/lee/mymicrophone/RecordingUI.java
+++ app/src/main/java/net/fandm/lee/mymicrophone/RecordingUI.java
@@ -2,8 +2,24 @@ package net.fandm.lee.mymicrophone;
 
 import android.media.MediaRecorder;
 
+import java.io.IOException;
+
 public class RecordingUI {
-    public RecordingUI(MediaRecorder recording){
-        
+
+
+    public void startRecording() {
+        MediaRecorder recording = new MediaRecorder();
+        recording.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recording.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        recording.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        recording.setAudioSamplingRate(44100);
+        recording.setAudioEncodingBitRate(192000);
+        try {
+            recording.prepare();
+            recording.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- app/src/main/java/net/fandm/lee/mymicrophone/VoiceMemo.java
+++ app/src/main/java/net/fandm/lee/mymicrophone/VoiceMemo.java
@@ -2,10 +2,14 @@ package net.fandm.lee.mymicrophone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class VoiceMemo extends AppCompatActivity {
@@ -16,14 +20,30 @@ public class VoiceMemo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voice_memo);
+        Button recordButton = (Button)findViewById(R.id.record_button);
+        recordButton.setOnClickListener(new View.OnClickListener(){
 
-        ArrayList<String> voice_recordings = new ArrayList<String>();
 
-        voice_recordings.add("hello");
-        voice_recordings.add("hello world");
-        ListView lv=(ListView)findViewById(R.id.list);
-        this.aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, voice_recordings);
-        lv.setAdapter(aa);
+            private ArrayAdapter<String> aa;
+
+            @Override
+            public void onClick(View v) {
+
+                RecordingUI recording = new RecordingUI();
+                recording.startRecording();
+                //ArrayList<RecordingUI> voice_recordings = new ArrayList<RecordingUI>();
+                ListView lv=(ListView)findViewById(R.id.list);
+                ArrayList<String> voice_recordings = new ArrayList<String>();
+                voice_recordings.add("hello world");
+                this.aa = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, voice_recordings);
+                //aa = new ArrayAdapter<RecordingUI>(getApplicationContext(), android.R.layout.simple_list_item_1, voice_recordings);
+                lv.setAdapter(aa);
+            }
+        });
+
+
+
+
 
     }
 }
