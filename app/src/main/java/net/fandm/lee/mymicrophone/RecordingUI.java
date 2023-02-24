be4976b5d56993e5413ed884910d4967f196e964
elee1109 <elee2@fandm.edu>
2023-02-24 14:32:45 -0500

    permissions/mic api config error

--- app/src/main/java/net/fandm/lee/mymicrophone/RecordingUI.java
+++ app/src/main/java/net/fandm/lee/mymicrophone/RecordingUI.java
@@ -1,25 +1,18 @@
 package net.fandm.lee.mymicrophone;
 
-import android.media.MediaRecorder;
-
-import java.io.IOException;
-
 public class RecordingUI {
 
+    private final String file_name;
 
-    public void startRecording() {
-        MediaRecorder recording = new MediaRecorder();
-        recording.setAudioSource(MediaRecorder.AudioSource.MIC);
-        recording.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-        recording.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-        recording.setAudioSamplingRate(44100);
-        recording.setAudioEncodingBitRate(192000);
-        try {
-            recording.prepare();
-            recording.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public RecordingUI(String file_location) {
+
+        this.file_name = file_location;
+
+    }
+    public String getFileName() {
+        return file_name;
+    }
+    public void setPlayButton() {
 
     }
 }
--- app/src/main/java/net/fandm/lee/mymicrophone/VoiceMemo.java
+++ app/src/main/java/net/fandm/lee/mymicrophone/VoiceMemo.java
@@ -1,49 +1,145 @@
 package net.fandm.lee.mymicrophone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.app.ActivityCompat;
 
 
+import android.app.ActivityManager;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class VoiceMemo extends AppCompatActivity {
 
-    private ArrayAdapter<String> aa;
+    private MediaRecorder recorder;
+    private boolean isRecording = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voice_memo);
-        Button recordButton = (Button)findViewById(R.id.record_button);
-        recordButton.setOnClickListener(new View.OnClickListener(){
+
+        ActivityCompat.requestPermissions(VoiceMemo.this, new String[]{
+                android.Manifest.permission.RECORD_AUDIO,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        }, 1);
 
 
-            private ArrayAdapter<String> aa;
+
+        Button recordButton = (Button) findViewById(R.id.record_button);
+        ListView lv = (ListView) findViewById(R.id.list);
+        recordButton.setOnClickListener(new View.OnClickListener() {
+
+
+            //private ArrayAdapter<String> aa;
 
             @Override
             public void onClick(View v) {
-
-                RecordingUI recording = new RecordingUI();
-                recording.startRecording();
-                //ArrayList<RecordingUI> voice_recordings = new ArrayList<RecordingUI>();
-                ListView lv=(ListView)findViewById(R.id.list);
-                ArrayList<String> voice_recordings = new ArrayList<String>();
-                voice_recordings.add("hello world");
-                this.aa = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, voice_recordings);
-                //aa = new ArrayAdapter<RecordingUI>(getApplicationContext(), android.R.layout.simple_list_item_1, voice_recordings);
-                lv.setAdapter(aa);
+                if (!isRecording) {
+                    startRecording();
+                    isRecording = true;
+                    recordButton.setForeground(AppCompatResources.getDrawable(getApplicationContext(), R.drawable.baseline_crop_square_24));
+                    recordButton.setBackgroundResource(R.drawable.red_stop);
+                } else {
+                    stopRecording();
+                    isRecording = false;
+                    recordButton.setForeground(AppCompatResources.getDrawable(getApplicationContext(), R.drawable.baseline_mic_24));
+                    recordButton.setBackgroundResource(R.drawable.record_bttn);
+                }
+                isRecording = !isRecording;
             }
         });
+    }
+
+            private void startRecording() {
+
+                String state = Environment.getExternalStorageState();
+
+                //check if external storage is mounted
+                if (!state.equals(Environment.MEDIA_MOUNTED)) {
+                    Toast.makeText(getApplicationContext(), "external storage not mounted", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                //create directory if it doesn't exist
+                File audioDir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+                File targetFile = new File(audioDir, "recorded_audio.mp4"); //this will be a time stamp?
+                recorder = new MediaRecorder();
+                Log.d("1", "media recorder created");
+
+                //prompt user for permissions
+
+
+
+
+                recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                Log.d("2", "AudioSource found (not permissions)");
+                recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+                Log.d("3", "OutputFormat found");
+
+                recorder.setOutputFile(targetFile.getAbsolutePath());
+                Log.d("4", "OutputFile found");
+
+                recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+                recorder.setAudioEncodingBitRate(1600);
+                recorder.setAudioSamplingRate(44100);
+                Log.d("5", "AudioEncoder found");
+
+
+
+                try {
+                    recorder.prepare();
+                    recorder.start();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.d("ERROR", "prepare() failed");
+                }
+
+            }
+
+                //ArrayList<RecordingUI> voice_recordings = new ArrayList<RecordingUI>();
+            private void stopRecording() {
+
+                recorder.stop();
+                recorder.release();
+                recorder = null;
+                Toast.makeText(getApplicationContext(), "recording stopped", Toast.LENGTH_LONG).show();
+            }
+
+            public void onPermissionCallback(int requestCode, String[] permissions, int[] grantResults) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "permission granted", Toast.LENGTH_LONG).show();
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "permission denied", Toast.LENGTH_LONG).show();
+                    String packageName = getApplicationContext().getPackageName(); //found on chatGPT3, how to kill app
+                    ActivityManager activityManager = (ActivityManager) getApplicationContext().getSystemService(ACTIVITY_SERVICE);
+                    if(activityManager != null) {
+                        activityManager.killBackgroundProcesses(packageName);
+                    }
+
+                }
+            }
+
 
 
 
 
 
     }
-}
+
